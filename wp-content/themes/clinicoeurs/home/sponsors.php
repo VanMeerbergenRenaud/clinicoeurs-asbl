093315a50f2ddfb9e83d160445fbd1ef325d9aae
Van Meerbergen Renaud <113695652+VanMeerbergenRenaud@users.noreply.github.com>
2023-07-07 19:02:36 +0200

    Sponsors section done

---
+++ wp-content/themes/clinicoeurs/home/sponsors.php
@@ -0,0 +1,23 @@
+<!-- Sponsors section -->
+<section class="sponsor" aria-label="Section des sponsors et partenaires">
+    <span class="sponsor__span"><?= get_field("sponsor-span"); ?></span>
+    <h2 role="heading" aria-level="2"><?= get_field("sponsor-title"); ?></h2>
+    <p><?= get_field("sponsor-text"); ?></p>
+    <!-- Gallery of sponsors -->
+    <div class="sponsor__gallery">
+        <?php
+        $images = get_field('sponsors');
+
+        if ($images) {
+            foreach ($images as $image) {
+                $image_url = $image['url'];
+                $image_alt = $image['alt'];
+
+                echo '<img src="' . $image_url . '" alt="' . $image_alt . '">';
+            }
+        }
+        ?>
+    </div>
+
+    <a href="#" class="cta">Voir tous les partenaires</a>
+</section>
