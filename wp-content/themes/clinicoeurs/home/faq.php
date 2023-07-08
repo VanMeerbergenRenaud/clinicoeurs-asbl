1116c243352c57ed13d9b33c14f4effe993d94fb
Van Meerbergen Renaud <113695652+VanMeerbergenRenaud@users.noreply.github.com>
2023-07-08 09:48:52 +0200

    Faq section done

--- wp-content/themes/clinicoeurs/home/faq.php
+++ wp-content/themes/clinicoeurs/home/faq.php
@@ -2,21 +2,19 @@
 <section class="faq" aria-label="Section des questions et réponses">
     <span class="faq__span"><?= get_field("faq-span"); ?></span>
     <h2 role="heading" aria-level="2"><?= get_field("faq-title"); ?></h2>
-    <div class="faq__container">
-        <ul>
-            <?php if (have_rows('faq-list')) : ?>
-                <?php while (have_rows('faq-list')) : the_row(); ?>
-                    <li>
-                        <input type="checkbox" name="accordion" id="question-<?php echo get_sub_field('id'); ?>">
-                        <label for="question-<?php echo get_sub_field('id'); ?>">
-                            <?php echo get_sub_field('title'); ?>
-                        </label>
-                        <p>
-                            <?php echo get_sub_field('text'); ?>
-                        </p>
-                    </li>
-                <?php endwhile; ?>
-            <?php endif; ?>
-        </ul>
-    </div>
+    <ul>
+        <?php if (have_rows('faq-list')) : ?>
+            <?php while (have_rows('faq-list')) : the_row(); ?>
+                <li>
+                    <input type="checkbox" name="accordion" id="question-<?php echo get_sub_field('id'); ?>">
+                    <label for="question-<?php echo get_sub_field('id'); ?>">
+                        <?php echo get_sub_field('title'); ?>
+                    </label>
+                    <p>
+                        <?php echo get_sub_field('text'); ?>
+                    </p>
+                </li>
+            <?php endwhile; ?>
+        <?php endif; ?>
+    </ul>
 </section>
