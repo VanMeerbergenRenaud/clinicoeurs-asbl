d2a92deb9c9274221a7229dd9da73fc22a41f0bf
Van Meerbergen Renaud <113695652+VanMeerbergenRenaud@users.noreply.github.com>
2023-07-12 17:24:11 +0200

    Donations page second refactoring

--- wp-content/themes/clinicoeurs/template-donations.php
+++ wp-content/themes/clinicoeurs/template-donations.php
@@ -1,13 +1,12 @@
 <?php /* Template Name: Donations page template */ ?>
 <?php get_header(); ?>
-
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
-
     <main>
         <?php
         // Fonction pour afficher les diverses sections ayant presque la même structure.
-        function display_section_with_list($section_name)
+        function display_section_structure($section_name): void
         {
+            $section_span = get_field($section_name . '-span');
             $section_title = get_field($section_name . '-title');
             $section_text = get_field($section_name . '-text');
             $section_img = get_field($section_name . '-img');
@@ -15,26 +14,37 @@
 
             ?>
             <section class="<?= $section_name ?>" aria-label="Section <?= $section_name ?>">
-                <span class="<?= $section_name ?>__span"><?= get_field($section_name . '-span'); ?></span>
-                <h2 role="heading" aria-level="2"><?= $section_title; ?></h2>
-                <p><?= $section_text; ?></p>
-                <img src="<?= $section_img; ?>" alt="Cliniclowns en groupe et joyeux">
-                <ul>
-                    <?php if (have_rows($list_name)) {
-                        while (have_rows($list_name)) {
-                            the_row();
+                <?php if ($section_span) : ?>
+                    <span class="<?= $section_name ?>__span"><?= get_field($section_name . '-span'); ?></span>
+                <?php endif; ?>
+
+                <?php if ($section_title) : ?>
+                    <h2 role="heading" aria-level="2"><?= $section_title; ?></h2>
+                <?php endif; ?>
+
+                <?php if ($section_text) : ?>
+                    <p><?= $section_text; ?></p>
+                <?php endif; ?>
+
+                <?php if ($section_img) : ?>
+                    <img src="<?= $section_img; ?>" alt="Cliniclowns en groupe et joyeux">
+                <?php endif; ?>
+
+                <?php if (have_rows($list_name)) : ?>
+                    <ul>
+                        <?php while (have_rows($list_name)) : the_row();
                             $title = get_sub_field('title');
                             $text = get_sub_field('text');
                             ?>
                             <li>
                                 <?= $title; ?>
-                                <p><?= $text; ?></p>
+                                <?php if ($text) : ?>
+                                    <p><?= $text; ?></p>
+                                <?php endif; ?>
                             </li>
-                            <?php
-                        }
-                    }
-                    ?>
-                </ul>
+                        <?php endwhile; ?>
+                    </ul>
+                <?php endif; ?>
             </section>
             <?php
         }
@@ -50,20 +60,15 @@
         </section>
         <?php
         // Section "operation"
-        display_section_with_list('operation');
+        display_section_structure('operation');
         // Section "benefits"
-        display_section_with_list('benefits');
-        ?>
-        <section class="details" aria-label="Section du processus d'une donation">
-            <span class="details__span"><?= get_field("details-span"); ?></span>
-            <h2 role="heading" aria-level="2"><?= get_field("details-title"); ?></h2>
-            <img src="<?= get_field("details-img"); ?>" alt="Cliniclowns en groupe et joyeux">
-        </section>
-        <?php
+        display_section_structure('benefits');
+        // Section "details"
+        display_section_structure('details');
         // Section "legislation"
-        display_section_with_list('legislation');
+        display_section_structure('legislation');
         // Section "legsDuo"
-        display_section_with_list('legsDuo');
+        display_section_structure('legsDuo');
         ?>
     </main>
 <?php endwhile; endif; ?>
