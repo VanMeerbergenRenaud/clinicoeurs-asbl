e45b6460655715ff32a998f3c16227333122623e
Van Meerbergen Renaud <113695652+VanMeerbergenRenaud@users.noreply.github.com>
2023-07-14 15:19:06 +0200

    details fixed

---
+++ wp-content/themes/clinicoeurs/otherParts/backToTop.php
@@ -0,0 +1,9 @@
+<a href="#" class="arrow top" title="Retour en haut de la page">&nbsp;
+    <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 34 34">
+        <g transform="translate(-1293 -605)">
+            <circle cx="17" cy="17" r="16" transform="translate(1293 605)" fill="#f0db62"/>
+            <path d="M11.594,7.565l.61.619a.674.674,0,0,1,0,.946L6.864,14.553a.65.65,0,0,1-.932,0L.591,9.13a.674.674,0,0,1,0-.946l.61-.619a.653.653,0,0,1,.943.011L5.3,10.938V2.92a.663.663,0,0,1,.659-.67h.879a.663.663,0,0,1,.659.67v8.018l3.155-3.362A.649.649,0,0,1,11.594,7.565Z"
+                  transform="translate(1303.602 613.75)" fill="#fdfdfc"/>
+        </g>
+    </svg>
+</a>
--- wp-content/themes/clinicoeurs/otherParts/callToAction.php
+++ wp-content/themes/clinicoeurs/otherParts/callToAction.php
@@ -1,10 +1,10 @@
 <section class="callToAction">
     <h2 role="heading" aria-level="2">Volontaire pour nous soutenir sous forme de don&nbsp;?</h2>
     <p>
-        N’hésitez pas à nous <a href="<?= get_home_url() . "/contact"; ?>">contacter</a> et à regarder notre
-        <a href="<?= get_home_url() . "/donations"; ?>">page de dons</a> expliquant tout ce que vous devez savoir.
+        N’hésitez pas à nous <a href="<?= get_home_url() . '/contact'; ?>">contacter</a> et à regarder notre
+        <a href="<?= get_home_url() . '/donations'; ?>">page de dons</a> expliquant tout ce que vous devez savoir.
     </p>
-    <a href="<?= get_home_url() . "/contact"; ?>" class="cta" title="Vers la page de contact">
+    <a href="<?= get_home_url() . '/contact' ?>" class="cta" title="Vers la page de contact">
         Nous contacter
         <span></span>
     </a>
